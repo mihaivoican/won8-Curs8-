cca20f01cce5238dbbc01464347f66ebb8503eb2
Mihai Voican <mihaivoican@yahoo.com>
2022-09-22 12:17:53 +0300

    commit Liste

---
+++ src/collections/ArrayClass.java
@@ -0,0 +1,14 @@
+package collections;
+
+public class ArrayClass {
+    public static void main(String[] args) {
+
+        //un array clasic
+        int[] numbers = {1, 2, 3};
+        System.out.println(numbers.length);
+        //alt array
+        int[] numc = new int[5];
+        System.out.println(numc.length);
+
+    }
+}
