6418211a389d2976155ec81059df565079e6a4cb
Mihai Voican <mihaivoican@yahoo.com>
2022-09-27 11:19:03 +0300

    Homework 9 ex 3

--- src/Homework9/ex3/Company.java
+++ src/Homework9/ex3/Company.java
@@ -1,10 +1,10 @@
 package Homework9.ex3;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Company {
-    private Set<Person> angajati = new TreeSet<Person>();
+
+    private Set<Person> angajati = new HashSet<Person>();
 
     public Company(Set<Person> angajati) {
         this.angajati = angajati;
@@ -18,6 +18,7 @@ public class Company {
         return angajati;
     }
 
+    //- getManager(): Person  - checks all persons and returns the one with position "manager"
     public Person getManager() {
         for (Person p : angajati) {
             if (p.getPosition().equals("manager")) {
@@ -25,6 +26,52 @@ public class Company {
             }
         }
         return new Person("-", 0, "-");
+    }
+
+    // getPersons(String profession):List<Person>  - returns all the persons that have the received proffesion
+    public List<Person> getPersonByProf(String profesia) {
+        List<Person> listaP = new ArrayList<Person>();
+        for (Person p : angajati) {
+            if (p.getPosition().equals(profesia)) {
+                listaP.add(p);
+            }
+        }
+        return listaP;
+    }
+
+    // getPersonsOlder(int age): List<Person> - returns all the persons that are older than received age
+
+    public List<Person> getPersonsOlder(int varsta) {
+        List<Person> listaP = new ArrayList<Person>();
+        for (Person p : angajati) {
+            if (p.getAge() >varsta) {
+                listaP.add(p);
+            }
+        }
+        return listaP;
+    }
+
+    // getPerson(String filterName):List<Person> - returns all the persons that have names that contains the received string
+    public List<Person> getPersonContainingString(String sir) {
+        List<Person> listaP = new ArrayList<Person>();
+        for (Person p : angajati) {
+            if (p.getName().contains(sir)) {
+                listaP.add(p);
+            }
+        }
+        return listaP;
+    }
+
+    //- employ(Person) - employs a person. Adds it to the list of persons
+    public Person employ(Person pers){
+        int nrAng =angajati.size();
+        angajati.add(pers);
+        if(angajati.size() >nrAng){
+            return pers;
+        }else {
+            return new Person("-",0,"-");
+        }
+
     }
 
 }
--- src/Homework9/ex3/Main.java
+++ src/Homework9/ex3/Main.java
@@ -1,23 +1,51 @@
 package Homework9.ex3;
 
-import java.util.TreeSet;
+import java.util.HashSet;
+import java.util.List;
+//import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
 
-        TreeSet<Person> lista = new TreeSet<Person>();
-        Person p1 = new Person("Big Boss",55,"manager");
-        Person p2 = new Person("Ionescu M",55,"welder");
-        Person p3 = new Person("Georgescu V",55,"carpenter");
-        Person p4 = new Person("Florescu V",55,"plummer");
+        HashSet<Person> lista = new HashSet<Person>();
+        Person p1 = new Person("Big Boss", 55, "manager");
+        Person p2 = new Person("Ionescu M", 25, "welder");
+        Person p3 = new Person("Georgescu V", 45, "carpenter");
+        Person p4 = new Person("Florescu V", 65, "plummer");
+        Person p5 = new Person("Vasilescu L", 50, "carpenter");
         lista.add(p1);
         lista.add(p2);
         lista.add(p3);
         lista.add(p4);
+        lista.add(p5);
 
         Company companie = new Company(lista);
 
-        System.out.println(String.format("Managerul este %s de %s ani",companie.getManager().getName(), companie.getManager().getAge()));
+        System.out.println(String.format("Managerul este %s de %s ani", companie.getManager().getName(), companie.getManager().getAge()));
 
+        //listam toti meseriasii de la o profesie
+        List<Person> listaPersCalificare = companie.getPersonByProf("carpenter");
+        for (Person p : listaPersCalificare) {
+            System.out.println(String.format("%s : Nume : %s, varsta %s ", "carpenter", p.getName(), p.getAge()));
+        }
+
+        //listam pers mai mari decat o varsta data
+        List<Person> listaPers = companie.getPersonsOlder(45);
+        for (Person p : listaPers) {
+            System.out.println(String.format("virsta minima %s : Nume : %s, varsta %s ", "45", p.getName(), p.getAge()));
+        }
+
+        //lista pers care contin in nume un sir dat
+        List<Person> listaPers2 = companie.getPersonContainingString("escu");
+        for (Person p : listaPers2) {
+            System.out.println(String.format(" Nume : %s, varsta %s ", p.getName(), p.getAge()));
+        }
+
+        //angajam o pers noua
+        Person p =new Person("Georgescu Adam",28,"strungar");
+
+        Person p6 = companie.employ(p);
+
+        System.out.println(String.format("A fost angajata persoana cu datele: Nume: %s, varsta: %s, profesia: %s",p6.getName(), p6.getAge(), p6.getPosition()));
     }
 }
