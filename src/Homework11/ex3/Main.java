7b1eed125e30eea57ece02afcf2ab90d6a332fe6
Mihai Voican <mihaivoican@yahoo.com>
2022-10-11 14:34:37 +0300

    homework 11, ex 3 complet

--- src/Homework11/ex3/Main.java
+++ src/Homework11/ex3/Main.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Employee emp = new Employee("Georgescu",25,"blond","RAR",5000);
+
         List<Employee> listaAngajati= new ArrayList<>();
         listaAngajati.add(emp);
         listaAngajati.add(new Employee("Ivanescu",44,"alb","Nokia",7000));
@@ -22,6 +23,15 @@ public class Main {
         System.out.println(listSalGreater(4999,listaAngajati));
 
 
+        //3.2 extract a Map that gathers all the persons for a company
+        System.out.println("******************************");
+        System.out.println(listEmpByComp(listaAngajati));
+
+        //3.3 calculate the sum of all salaries for all employees
+        System.out.println("Suma salariilor e: " + sumOfSalaries(listaAngajati));
+
+        //3.4 calculate which company gives the biggest salary
+        System.out.println("Compania cu cel mai mare salariu este.... " + bigestCompSalary(listaAngajati));
     }
 
 
@@ -33,6 +43,35 @@ public class Main {
             }
         }
         return result;
+    }
+
+    public static Map<String,List<Employee>> listEmpByComp(List<Employee> emps){
+        Map<String,List<Employee>> result = new HashMap<>();
+        for (Employee e:emps){
+            result.putIfAbsent(e.getCompany(),new ArrayList());
+            result.get(e.getCompany()).add(e);
+        }
+        return result;
+    }
+
+    public static Integer sumOfSalaries(List<Employee> emps){
+        Integer suma =0;
+        for (Employee e:emps){
+            suma += e.getSalary();
+        }
+        return suma;
+    }
+
+    public static String bigestCompSalary(List<Employee> emps){
+        String comp = null;
+        Integer sal = null;
+        for (Employee e:emps){
+            if (sal == null || sal < e.getSalary()){
+                sal=e.getSalary();
+                comp = e.getCompany();
+            }
+        }
+        return comp;
     }
 
 }
